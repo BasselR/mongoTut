93d59158cb3f11711cf2ae00661d1d2acfc0ec29
Bassel <mail2bassel@yahoo.com>
2020-08-01 16:43:47 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./DB/Connection')
+
+connectDB();
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+});
